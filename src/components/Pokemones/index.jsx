5b96f7b7eeb5ea05e24d3829cbdbeeb7f504187a
William_ws7 <alexander171296@gmail.com>
2020-06-26 00:42:34 -0400

    added reducer

--- src/components/Pokemones/index.jsx
+++ src/components/Pokemones/index.jsx
@@ -2,8 +2,23 @@ import React, { Component } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { obtenerPokemonesAccion } from "../../redux/pokeDucks";
 const Pokemones = () => {
-  const dispatch = useDispatch(obtenerPokemonesAccion);
-  return <div></div>;
+  const dispatch = useDispatch();
+  const pokemones = useSelector((store) => store.pokemones);
+  console.log(pokemones);
+  function getData() {
+    dispatch(obtenerPokemonesAccion());
+  }
+  return (
+    <div>
+      Lista de Pokemones
+      <button onClick={getData}>Get Pokemones</button>
+      <ul>
+        {pokemones.map((item) => (
+          <li key={item.name}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Pokemones;
